01d8f0b8b28c110a270e7c32d148c2f944fd9d1f
4lireza <4lirezajalali@gmail.com>
2023-06-17 08:07:30 +0330

    Initial commit message

---
+++ convert/SSExtract.js
@@ -0,0 +1,141 @@
+const {
+    isProtecolSupported,
+    isObfsSupported,
+    isCypherSupported,
+    jsonEncode,
+    consoleLog,
+    parseURI2ClashData,
+} = require("./conversions");
+
+
+function extractSsAndSsr(text){
+
+	let matchesIt=text.matchAll(/(?<URI>((ssr?)|vmess):\/\/[^<>\n\s]+)/ig);
+	
+	let proxyList=[];
+	
+	for(let m of matchesIt){
+		let uri=m.groups.URI;
+        let data;
+		try{
+            data=parseURI2ClashData(uri,true);
+        }catch(e){
+            consoleLog(`Error: Fail to parse URI of ${uri}.\n [${e.message}] \n`);
+            console.error(e);
+        }
+		
+		if(!data){
+			continue;
+		}
+		
+		if(data['type']=="ssr"){
+			if(!data['protocol']){
+				consoleLog("Warining: skip due to empty protecol of "+m[0]+"\n");
+				continue;
+			}
+			
+			
+			if(!isProtecolSupported(data['protocol'])){
+				consoleLog("Warining: skip due to unsupported protocol of "+data['protocol']);
+				continue;
+			}
+			
+			if(!isObfsSupported(data['obfs'])){
+				consoleLog("skip due to unsupported obfs!");
+				continue;
+			}
+			
+			if(!isCypherSupported(data['cipher'])){
+				consoleLog("Warining: skip due to unsupported cipher of "+data['cipher']);
+				continue;
+			}
+		}
+		
+		data.name=data['type']+"_"+proxyList.length;
+		if(proxyList.findIndex(v2=>(v2.server===data.server))==-1){
+			proxyList.push(data);
+		}
+	}
+	
+	return proxyList;
+}
+
+function extractTls(text){
+
+	let matchesIt=text.matchAll(/HTTPS\s+(?<server>[\w\.\-]+)\:(?<port>\d+)/ig);
+	
+	let proxyList=[];
+	
+	for(let m of matchesIt){
+		
+		let data=m.groups;
+		
+		if(!data){
+			continue;
+		}
+		
+		data.name="TLS_"+proxyList.length;
+		data.type="http";
+		data.tls=true;
+		if(proxyList.findIndex(v2=>(v2.server===data.server))==-1){
+			proxyList.push(data);
+		}
+		
+	}
+		
+	return proxyList;
+}
+
+function serializeForClacsh(proxyList){
+	let ymlClashDump="proxies:\n";
+	
+	for(let p of proxyList){
+		try{
+			let jsonStr=jsonEncode(p);
+			ymlClashDump+="  - "+jsonStr+"\n";
+		}catch(e){
+			consoleLog(`skiping due to encode error: ${e.getMessage()}`);
+			//throw $e;
+		}
+		
+		
+	}
+	
+	ymlClashDump+="\nproxies-names:\n";
+	for(let p of proxyList){
+		try{
+			ymlClashDump+="  - "+p.name+"\n";
+		}catch(e){
+			consoleLog(`skiping due to encode error: ${e.getMessage()}`);
+			//throw $e;
+		}
+		
+		
+	}
+	
+	return ymlClashDump;
+}
+
+function doConvert(MyProxies){
+	let rawInput= MyProxies;
+    let convertResult="";
+    let proxies=extractSsAndSsr(rawInput,true);
+        proxies=proxies.concat(extractTls(rawInput));
+        if(proxies.length<1){
+            consoleLog("Error: No entry found. Exiting...");
+            return;
+        }
+
+        convertResult=serializeForClacsh(proxies);
+
+	return convertResult;
+}
+
+
+
+module.exports = {
+    doConvert,
+    extractSsAndSsr,
+    extractTls,
+    serializeForClacsh,
+}
---
+++ index.js
@@ -0,0 +1,169 @@
+const fs = require("fs");
+const readline = require("readline");
+const spawn = require("child_process").spawn;
+var cron = require("node-cron");
+const http = require("http");
+const doConvert = require("./convert/SSExtract").doConvert
+require('dotenv').config()
+
+const token = process.env.TOKEN;
+let isChecking = false;
+
+async function processLineByLine(filePath) {
+  const fileStream = fs.createReadStream(filePath);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  const lines = [];
+  for await (const line of rl) {
+    lines.push(line);
+  }
+
+  return lines;
+}
+
+const spawnTest = async (url, last = false) => {
+  return new Promise((resolve, reject) => {
+    fs.chmodSync(__dirname + `/lite/lite`, 0o777);
+    // spawn(`bash -c "chmod 777 ${__dirname}/lite/lite"`);
+    let ls = spawn(`bash -c "${__dirname}/lite/lite --config config.json --test ${url}"`, { shell: true, cwd: __dirname + "/lite" });
+    ls.on("error", function (err) {
+      console.log("ls error", err);
+    });
+
+    ls.stdout.on("data", function (data) {
+      console.log("stdout: " + data);
+    });
+
+    ls.stderr.on("data", function (data) {
+      // console.log("stderr: " + data);
+    });
+
+    ls.on("close", async function (code) {
+      console.log("Checking Urls Proxies exited with code " + code + ` , url : ${url}`);
+      if (code == 0) {
+        console.log("code is 0", last);
+        const result = await fs.readFile(__dirname + "/lite/output.txt", "utf-8", async (err, data) => {
+          if (err) throw err;
+
+          await fs.writeFile( __dirname +"/proxies.txt", data, { flag: "a" }, (error) => {
+            if (error) {
+              console.log("file write error", error);
+              reject();
+            } else {
+              console.log("File written");
+              if (last == true) {
+                fs.readFile(__dirname + "/proxies.txt", "utf-8", async (err, data) => {
+                  if (err) throw err;
+                  const results = doConvert(data)
+                  fs.readFile(__dirname + "/clashConfig.txt", "utf-8", async (err, headers) => {
+                    if (err) throw err;
+                    await fs.writeFile( __dirname +"/clash.yaml", headers, { flag: "a" }, (error) => {
+                      if (error) throw error;
+                      fs.writeFile( __dirname +"/clash.yaml", results || "", { flag: "a" }, (error) => {
+                        if (error) throw error;
+                      })
+                    })
+                  })
+                })
+
+
+                let gitpush = spawn(`git add . && git commit -m "updateProxies" && git push https://${token}@github.com/4lirexa/ProxyCheckerArm`, { shell: true, cwd: __dirname });
+                gitpush.on("error", function (err) {
+                  console.log("gitpush error", err);
+                  reject();
+                });
+
+                gitpush.stdout.on("data", function (data) {
+                  console.log("stdout: " + data);
+                });
+
+                gitpush.stderr.on("data", function (data) {
+                  console.log("stderr: " + data);
+                });
+
+                gitpush.on("close", (code) => {
+                  resolve();
+                  console.log("finished all promises");
+                  console.log("git push exited with code " + code);
+                  isChecking = false;
+                });
+              }else{
+                resolve()
+              }
+            }
+          });
+        });
+      } else {
+        console.log("rejected");
+        reject();
+      }
+    });
+  });
+};
+
+async function getNodes() {
+  let gitpull = spawn(`git stash && git pull https://${token}@github.com/4lirexa/ProxyCheckerArm`, { shell: true, cwd: __dirname });
+  gitpull.on("error", function (err) {
+    console.log("gitpull error", err);
+  });
+
+  gitpull.stdout.on("data", function (data) {
+    console.log("stdout: " + data);
+  });
+
+  gitpull.stderr.on("data", function (data) {
+    console.log("stderr: " + data);
+  });
+
+  gitpull.on("close", async function (code) {
+    console.log("git pull exited with code " + code);
+    if (code == 0) {
+      isChecking = true;
+      fs.writeFile( __dirname + "/proxies.txt", "", (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("proxies Empty!");
+        }
+      });
+      const urls = await processLineByLine(__dirname + "/nodes.txt");
+
+      let i = 1;
+      for (const url of urls) {
+        console.log("checking -> " + url);
+        await spawnTest(url, i == urls.length).catch(err=>console.log(err));
+        i++;
+      }
+    }
+  });
+}
+
+getNodes();
+cron.schedule("* 1 * * *", () => {
+  if (isChecking == false) {
+    getNodes();
+  }
+});
+
+const server = http.createServer((req, res) => {
+  if(req.url == "/"){
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    const data = fs.readFileSync(__dirname + "/proxies.txt", "utf-8");
+    res.end(data);
+  }
+  if(req.url == "/clash"){
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    const data = fs.readFileSync(__dirname + "/clash.yaml", "utf-8");
+    res.end(data);
+  }
+});
+
+server.listen(3000, () => {
+  console.log("http running on port 3000");
+});
